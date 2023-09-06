99427d3d31a6aa775653e729219a3851e09db91b
taylorbrookes <144271566+taylorbrookes@users.noreply.github.com>
2023-09-06 14:22:30 -0400

    Create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,20 @@
+public interface INumber<T> {
+  T plus(T input);
+  T minus(T input);
+  T divide(T input);
+  T multiply(T input);
+  void print();
+}
+
+public class Fraction implements INumber {
+  //numerator
+  //denominator
+}
+
+//method hasSameValue
+
+//define Array of Fractions
+
+//find most frequent value in array
+//implement inserion sort
+//implement recursive method 
